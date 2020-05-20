12b2537d0bd6e1afb2b5b581ced0024cb8440acb
Maksym Skorchenko <a2800sma@gmail.com>
2020-05-20 12:37:37 +0300

    Filled readme, commented public methods, activation and derivative function extracted from the constructor

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# NN4J
-Simple Implementation of Neural Network on Java
+# The SImple implementation of the Neural Network for Java
+
+
+## Features:
+1. Mutation
+2. Crossing
+3. Education by back propagation method
+4. Unlimited Network depth
+5. Unlimited Network width
+6. Individual width of the each Layer of the Network
+
+## Usage
+
+## Example project
+### Simulation of evolution 
+https://github.com/Fastto/SimulationOfEvolution. 
+
+It implements genetic algorithm with crossing and mutation.
+
+### Separation the surface by color zones based on several points
+IN PROGRESS
+
+The implementation uses propagation method of education
--- src/main/java/com/owldevs/nn4j/NeuralNetwork.java
+++ src/main/java/com/owldevs/nn4j/NeuralNetwork.java
@@ -4,24 +4,33 @@ import java.util.function.UnaryOperator;
 
 /**
  * The simple implementation of the multilayer perceptron.
+ * Features:
+ *     1. Mutation
+ *     2. Crossing
+ *     3. Education by back propagation method
+ *     4. Unlimited Network depth
+ *     5. Unlimited Network width
+ *     6. Individual width of the each Layer of the Network
  */
 public class NeuralNetwork implements Cloneable {
-
     private double learningRate;
     private Layer[] layers;
-    private UnaryOperator<Double> activation;
-    private UnaryOperator<Double> derivative;
+    private UnaryOperator<Double> activation = x -> 1 / (1 + Math.exp(-x));
+    private UnaryOperator<Double> derivative = x -> x * (1 - x);
 
     /**
      * Creating new NN with random weights and biases
      *
-     * @param learningRate 0..1
-     * @param sizes
+     * @param learningRate 0..1 The rate of the learning. Makes sense with using backPropagation method for Network
+     *                     education. If you do not educate network just set any value here.
+     * @param sizes The array which represents quantity of the layers and width of the each layer of the Network.
+     *              For example if sizes = [2, 5, 3] will be crated perceptron with 3 layers.
+     *              1st layer (inputs) width is 2
+     *              2nd layer (hidden layers) width is 5
+     *              3rd layer (outputs) width is 3
      */
     public NeuralNetwork(double learningRate, int... sizes) {
         this.learningRate = learningRate;
-        this.activation = xx -> 1 / (1 + Math.exp(-xx));
-        this.derivative = xx -> xx * (1 - xx);
 
         layers = new Layer[sizes.length];
         for (int i = 0; i < sizes.length; i++) {
@@ -38,22 +47,22 @@ public class NeuralNetwork implements Cloneable {
     }
 
     /**
-     * Creating new NN with preconfigured weights and biases.
+     * Creating new Network with preconfigured weights and biases.
      *
-     * @param learningRate 0..1
-     * @param layers
+     * @param learningRate 0..1 The rate of the learning. Makes sense with using backPropagation method for Network
+     *                     education. If you do not educate network just set any value here.
+     * @param layers The array of the layers of the Network
      */
     public NeuralNetwork (double learningRate, Layer[] layers) {
         this.learningRate = learningRate;
-        this.activation = x -> 1 / (1 + Math.exp(-x));
-        this.derivative = y -> y * (1 - y);
         this.layers = layers;
     }
 
     /**
-     *
-     * @param inputs
-     * @return
+     * Push signals to input of Network and receive Network calculation result from the Network output
+     * @param inputs Array of params. The size of the array has to be more or equal
+     *               the size of the 1st Layer of the Network.
+     * @return Network output values. Size equals to the size og the last layer of the Network
      */
     public double[] feedForward(double[] inputs) {
         System.arraycopy(inputs, 0, layers[0].neurons, 0, inputs.length);
@@ -73,8 +82,12 @@ public class NeuralNetwork implements Cloneable {
     }
 
     /**
+     * Educate Network by back propagation method.
+     * This method has to be called right after calling feedForward.
+     * Targets param shows the Network which result it has to associate with
+     * input values which came in feedForward()
      *
-     * @param targets
+     * @param targets The proper values of output which should correspond to las call of feedForward()
      */
     public void backPropagation(double[] targets) {
         double[] errors = new double[layers[layers.length - 1].size];
@@ -118,8 +131,12 @@ public class NeuralNetwork implements Cloneable {
     }
 
     /**
-     *
-     * @param rate 0..1
+     * Mutate the current instance with specified chance and rate
+     * @param rate Rate set the maximal possible delta on which the value of
+     *             Weights and Biases can be modified to both sides -+.
+     *             The range of values of Weights and Biases is from -1 to 1.
+     *             Thar is why the limit of range is 2, but we recommend
+     *             do use rate value between 0 and 0.2. It makes better results.
      * @param chance 0..1 where 1 means 100%
      */
     public void mutate(double rate, double chance) {
@@ -149,39 +166,37 @@ public class NeuralNetwork implements Cloneable {
     }
 
     /**
-     * Crossing over current Network with the target.
+     * Cross the current Network with the target.
      *
-     * @param nn
+     * @param targetNetwork The target instance of NeuralNetwork
      */
-    public void crossover(NeuralNetwork nn)
+    public void crossWith(NeuralNetwork targetNetwork)
     {
-        Layer[] lr = nn.layers.clone();
+        Layer[] layer = targetNetwork.layers.clone();
         for (int i = 0; i < layers.length; i++){
             for(int j = 0; j < layers[i].weights.length; j++) {
                 for(int g = 0; g < layers[i].weights[j].length; g++) {
                     if (Math.random() < 0.5) {
-                        layers[i].weights[j][g] = lr[i].weights[j][g];
+                        layers[i].weights[j][g] = layer[i].weights[j][g];
                     }
                 }
             }
-
             for(int j = 0; j < layers[i].biases.length; j++) {
                 if (Math.random() < 0.5) {
-                    layers[i].biases[j] = lr[i].biases[j];
+                    layers[i].biases[j] = layer[i].biases[j];
                 }
             }
         }
     }
 
     /**
-     * Clonning the current Network
+     * Clone of the current NeuralNetwork instance
      *
-     * @return
+     * @return new instance of the NeuralNetwork
      */
     @Override
-    public NeuralNetwork clone(){
+    public NeuralNetwork clone() {
         Layer[] newLayers = new Layer[layers.length];
-
         for (int i = 0; i < layers.length; i++) {
             int nextSize = 0;
             if (i < layers.length - 1) nextSize = layers[i + 1].size;
@@ -197,5 +212,24 @@ public class NeuralNetwork implements Cloneable {
         return new NeuralNetwork(learningRate, newLayers);
     }
 
+    /**
+     * Override default Activation Function
+     * @param activation Activation Function
+     * @return this
+     */
+    public NeuralNetwork setActivationFunction(UnaryOperator<Double> activation) {
+        this.activation = activation;
+        return this;
+    }
+
+    /**
+     * Override default Derivative Function
+     * @param derivative Derivative Function
+     * @return this
+     */
+    public NeuralNetwork setDerivativeFunction(UnaryOperator<Double> derivative) {
+        this.derivative = derivative;
+        return this;
+    }
 
 }
